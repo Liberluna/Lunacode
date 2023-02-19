ebba70541329841dcebfe1e41d2af2a9ec51a0a2
nakasyou <nakasyou1103@gmail.com>
2023-02-19 16:30:53 +0900

    fix change

---
+++ build.ts
@@ -0,0 +1,14 @@
+import * as esbuild from "https://deno.land/x/esbuild@v0.17.8/mod.js";
+
+console.log(`ESBuild at ${esbuild.version}`);
+const esm=await esbuild.context({
+  entryPoints: JSON.parse(await Deno.readTextFile("entry.json")).entrypoints,
+  outdir: './build',
+  format:"esm",
+});
+const args=Deno.args;
+if(args.includes("--watch")){
+  await esm.watch()
+}else{
+  const rebuild=await esm.rebuild();
+}
